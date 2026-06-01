9268f659c82226b2c4e2d96818b6b3a10c4a94c4
omarashrafayad <omar.ayad3040@gmail.com>
2026-06-01 12:42:48 +0300

    fix: remove erroneous selected attribute from class list page length dropdown

--- src/app/(dashboard)/class-list/page.tsx
+++ src/app/(dashboard)/class-list/page.tsx
@@ -74,7 +74,7 @@ export default function ClassListPage() {
                                     <select name="dataTable_length" aria-controls="dataTable"
                                         className="dt-input form-control form-select">
                                         <option value="5">5</option>
-                                        <option value="10" selected>10</option>
+                                        <option value="10">10</option>
                                         <option value="25">25</option>
                                         <option value="50">50</option>
                                         <option value="100">100</option>
